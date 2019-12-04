7bf12fe03adc15d83faed860493c39973ed70c53
jeffrey osayande <sajeff2003@yahoo.com>
2019-12-03 20:12:33 -0500

    testr

---
+++ UMS/Models/usercontext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UMS.Models
+{
+    public class usercontext : DbContext
+    {
+        public usercontext(DbContextOptions<usercontext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<user> user { get; set; }
+    }
+}
